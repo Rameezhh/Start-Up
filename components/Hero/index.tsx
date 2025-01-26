1a2f1769c85c8de7fffdf8decfd73cffd0bd7d2f
Rameez <103438608+Rameezhh@users.noreply.github.com>
2025-01-26 17:39:12 +0500

    Update index.tsx

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -15,11 +15,11 @@ const Hero = () => {
                 data-wow-delay=".2s"
               >
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                  Free and Open-Source for Startup & SaaS
+                  Best HMD Startup site for clients visualization
                 </h1>
                 <p className="dark:text-body-color-dark mb-12 text-base !leading-relaxed text-body-color sm:text-lg md:text-xl">
-                  Startup is free SaaS business websites comes with all the essential pages,
-                  components, and sections you need to launch a complete
+                  Startup is the Demo visualization business website for client estimation and design comes with all the essential pages,
+                  components, and sections you need to launch in complete
                   business website.
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
@@ -27,13 +27,13 @@ const Hero = () => {
                     href="/"
                     className="rounded-sm bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
-                    🔥 Get Pro
+                    🔥 For More
                   </Link>
                   <Link
                     href="/"
                     className="inline-block rounded-sm bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
                   >
-                    Star on GitHub
+                   Get a Star
                   </Link>
                 </div>
               </div>
